df7f55cde17ad196c458ea8263701e6f23990024
shershen21 <67759297+shershen21@users.noreply.github.com>
2025-03-03 20:32:27 +0400

    v0.11-4

--- common.py
+++ common.py
@@ -7,5 +7,7 @@ def generate_filename(length: int) -> str:
     return ''.join(random.choices(string.ascii_letters + string.digits, k=length))
 
 
-def run_threaded(func, *args, **kwargs):
-    threading.Thread(target=func, args=args, kwargs=kwargs).start()
+def run_threaded(target, *args, **kwargs):
+    thread = threading.Thread(target=target, args=args, kwargs=kwargs)
+    thread.start()
+    return thread
